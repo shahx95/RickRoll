e9e3bed7c0e5ef492e917e4d2c7d16f17a4f9a8d
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-01-21 06:41:01 +0500

    Add rickroll script

---
+++ script.js
@@ -0,0 +1,7 @@
+const neverGonnaGiveYouUp = "/watch?v=dQw4w9WgXcQ"
+
+const vidLink = document.getElementsByClassName('yt-simple-endpoint')
+
+for (link of vidLink) {
+link.href = neverGonnaGiveYouUp;
+}
